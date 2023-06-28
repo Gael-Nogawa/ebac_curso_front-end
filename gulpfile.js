561af1f392fb805d0533025ebb0269183bc3d5bc
Gael Eiji Nogawa <gael.nogawa@gmail.com>
2023-06-28 16:51:05 -0300

    Docs: gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -29,7 +29,7 @@ function compilaSass(){
         .pipe(gulp.dest('./build/styles'));
 }
 
-exports.watch= function(){
+exports.default = function(){
     gulp.watch('./source/styles/*.scss',{ ignoreInitial: false }, gulp.series(compilaSass));
     gulp.watch('./source/images/*',{ ignoreInitial: false }, gulp.series(comprimeImagens));
     gulp.watch('./source/scripts/*.js', { ignoreInitial: false }, gulp.series(comprimeJavaScript));
